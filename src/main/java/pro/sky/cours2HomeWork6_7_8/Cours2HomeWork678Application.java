4e0451a1ad8a9c3d40424d2887356c9fa0415565
pavel_gorpynyak <pavel_gorpynyak@bk.ru>
2022-08-13 10:20:13 +0300

    First commit

---
+++ src/main/java/pro/sky/cours2HomeWork6_7_8/Cours2HomeWork678Application.java
@@ -0,0 +1,13 @@
+package pro.sky.cours2HomeWork6_7_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cours2HomeWork678Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cours2HomeWork678Application.class, args);
+	}
+
+}
