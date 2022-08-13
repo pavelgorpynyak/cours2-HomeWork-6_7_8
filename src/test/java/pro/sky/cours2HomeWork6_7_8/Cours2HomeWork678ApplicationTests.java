1827f52aa40e3dbc2d723afba6d86c469d7d00ea
pavel_gorpynyak <pavel_gorpynyak@bk.ru>
2022-08-13 10:20:42 +0300

    First commit

---
+++ src/test/java/pro/sky/cours2HomeWork6_7_8/Cours2HomeWork678ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cours2HomeWork6_7_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cours2HomeWork678ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
